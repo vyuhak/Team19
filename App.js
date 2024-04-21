f213711634d1b3726ce54d1ba76852be0c0325e4
vyuhak <kandulva@mail.uc.edu>
2024-04-21 19:06:07 -0400

    update

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
 
-  
+
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Login' }} />
